6eadfa961f6994c06a55f2e9d8c789ae91759bcf
Administrator <335075334@qq.com>
2020-04-11 21:03:46 +0800

    商品模块

--- src/views/product/product/components/ProductInfoDetail.vue
+++ src/views/product/product/components/ProductInfoDetail.vue
@@ -1,6 +1,13 @@
 <template>
   <div style="margin-top: 50px">
     <el-form :model="value" :rules="rules" ref="productInfoForm" label-width="120px" style="width: 600px" size="small">
+      <el-form-item label="商品分类：" prop="productTypeId">
+        <el-cascader
+          clearable
+          v-model="selectProductTypeValue"
+          :options="productTypeOptions">
+        </el-cascader>
+      </el-form-item>
       <el-form-item label="商品名称：" prop="productName">
         <el-input v-model="value.productName"></el-input>
       </el-form-item>
@@ -32,8 +39,8 @@
 
 <script>
 
-  import {deletePic} from '@/mall-api/product/product';
   import MultiUpload from '@/components/Upload/multiUpload'
+  import {getProductTypeCascader} from '@/mall-api/product/productType'
 
   export default {
     name: "ProductInfoDetail",
@@ -47,6 +54,8 @@
     },
     data() {
       return {
+        selectProductTypeValue: [],
+        productTypeOptions: [],
         picDetailList: [],
         picUrlList: [],
         rules: {
@@ -59,6 +68,9 @@
         }
       };
     },
+    created() {
+      this.getProductTypeList();
+    },
     watch:{
       picUrlList: function (newValue) {
         this.value.picUrl=newValue[0];
@@ -66,8 +78,38 @@
       picDetailList: function (newValue) {
         this.value.picList=newValue;
       },
+      selectProductTypeValue: function (newValue) {
+        if (newValue.length != 2) {
+          this.value.productTypeId = null;
+          this.productTypeId = null;
+          this.$message({
+            message: '只能选择第二级分类信息',
+            type: 'error',
+            duration: 1000
+          });
+          return false;
+        }
+        this.value.productTypeId = newValue[1];
+        this.productTypeId = newValue[1];
+      },
     },
     methods: {
+      //获取分类信息
+      getProductTypeList() {
+        getProductTypeCascader().then(response => {
+          let list = response.data;
+          this.productTypeOptions = [];
+          for (let i = 0; i < list.length; i++) {
+            let children = [];
+            if (list[i].children != null && list[i].children.length > 0) {
+              for (let j = 0; j < list[i].children.length; j++) {
+                children.push({label: list[i].children[j].label, value: list[i].children[j].value});
+              }
+            }
+            this.productTypeOptions.push({label: list[i].label, value: list[i].value, children: children});
+          }
+        });
+      },
       //下一步
       handleNext(formName) {
         this.$refs[formName].validate((valid) => {
@@ -88,6 +130,9 @@
         if (this.isEdit) {
           this.picUrlList.push(value.picUrl);
           this.picDetailList = value.picList;
+          this.selectProductTypeValue.push(value.productTypeParentId.toString());
+          this.selectProductTypeValue.push(value.productTypeId.toString());
+          this.getProductTypeList();
           this.$refs.picUrlMultiUpload.initUpdateDate(this.picUrlList)
           this.$refs.picDetailMultiUpload.initUpdateDate(this.picDetailList)
         }
